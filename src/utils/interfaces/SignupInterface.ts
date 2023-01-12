01e5f922266c5588550164b78c470a775b5d22da
alekseikorotkov <korotkovaleksei@icloud.com>
2023-01-12 19:01:59 +0200

    Implement interfaces for work with forms

--- src/utils/interfaces/SignupInterface.ts
+++ src/utils/interfaces/SignupInterface.ts
@@ -1,1 +1,26 @@
-export interface ISignupValues {}
+export interface ISignupValues
+  extends IFirstStepSU,
+    ISecondStepSU,
+    IThirdStepSU {}
+import dayjs, { Dayjs } from "dayjs";
+export interface IFirstStepSU {
+  numberPhone: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  DOB: Dayjs | null;
+  conditions: boolean;
+}
+export interface ISecondStepSU {
+  password: string;
+  confirmPassword: string;
+}
+export interface ITimeSlot {
+  from: string;
+  to: string;
+}
+type Arr<T> = [T, ...T[]];
+export interface IThirdStepSU {
+  position: string;
+  timeSlots?: Arr<ITimeSlot>;
+}
